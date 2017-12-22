eff3158dc6d88c5ba69c3a646db87c3249750c02
Muhammad Albie Al Hasan <alhasan99dev@gmail.com>
2017-12-22 11:54:11 +0800

    Add files via upload
    home part of hasandev.kelasmuhammad.com

---
+++ content_home.php
@@ -0,0 +1,195 @@
+<style>
+
+	.boxFun {
+		max-width: 100%;
+	    min-height: 200px;
+	    background: #25b14c;
+	    text-align: center;
+	    line-height: 200px;
+	    border: 4px solid #222;
+	    box-sizing: border-box;
+	    color: #eee;
+	}
+	.boxFun span {
+		font-size: 50px;
+	    font-weight: 800;
+	    display: inline-block;
+	    margin: auto;
+	    width: 100%;
+	    line-height: 1.5;
+	    vertical-align: middle;
+	}
+
+	.boxControl {
+	    max-width: 100%;
+	    min-height: 50px;
+	    background: #222;
+	    line-height: 50px;
+	    text-align: center;
+	    padding-bottom: 10px;
+	}
+
+	.boxControl .btnChangeTxt {
+       	height: 28px;
+	    width: 100px;
+	    color: #eee;
+	    font-weight: 800;
+	    background: #25b14c;
+	    border: none;
+	    border-radius: 3px;
+	    cursor: pointer;
+	    box-shadow: 1px 2px 2px #227d3c;
+	}
+	.boxControl input {
+		height: 24px;
+	    padding: 2px 10px;
+	    border: none;
+	    border-radius: 2px;
+	}
+	.boxControl .inpChangeTxt {
+	    width: 300px;
+	}
+
+	.boxControl .inpChangeClrBg, .boxControl .inpChangeClrTxt {
+	    width: 150px;
+	}
+	.boxControl .btnChangeTxt:visited {
+		outline: none;
+	}
+
+	.boxControl .btnChangeTxt:hover {
+		background: #259043;
+		box-shadow: none;
+		font-weight: 200;
+		outline: none;
+	}
+	@media screen and (max-width: 460px){
+		.boxFun span {
+			font-size: 40px;
+		}
+
+		.boxControl .btnChangeTxt {
+			display: block;
+			margin: 0 auto;
+		}
+
+		.boxControl .inpChangeTxt {
+			display: inline-block;
+			width: 80%;
+		}
+
+		.boxControl .inpChangeClrTxt {
+			display: inline-block;
+			width: 80%;
+		}
+		
+		.boxControl .inpChangeClrBg {
+			display: inline-block;
+			width: 80%;
+		}
+	}
+	@media screen and (max-width: 350px){
+		.boxFun span {
+			font-size: 30px;
+		}
+	}
+</style>
+<h2>Welcome to My Website</h2>
+
+<div class="boxFun">
+	<span>hello world!</span>
+</div>
+
+<div class="boxControl">
+	<!-- <form method="GET" class="changeTxt"> -->
+		<input type="text" class="inpChangeClrBg" placeholder="#warna background">
+		<input type="text" class="inpChangeClrTxt" placeholder="#warna text">
+		<input class="inpChangeTxt" type="text" placeholder="ubah text">
+		<input type="submit" name="" class="btnChangeTxt" value="coba">
+	<!-- </form> -->
+</div>
+
+<div class="salam"><p class="salam"><span class="italic">Assalamu'alaikum Warahmatullahi Wabarakatuh ....</span><span class="arabic" dir="rtl" lang="ar">السلام عليكم ورحمة ا للة و بركاتة</span></p>
+</div>
+<p>Selamat datang di website saya yang sederhana, ini adalah website pertama yang saya buat sendiri full tanpa pake template punya orang lain atau template bawaan aplikasi seperti dreamweaver, dan website pertama saya yang terhosting berada di internet selain dari <a href="https://hasandev.blogspot.co.id" target="_blank">blog saya</a> yang numpang di tempat <a href="https://www.blogger.com" target="_blank">(blogger.com)</a> punyanya mbah google. Walau masih sangat sederhana dan banyak kekurangannya, tapi biarlah ini menjadi awal dari perjalanan saya menjadi web developer professional. amiin... ya Allah.</p>
+
+<h3>Website apasih ini ?</h3>
+<p>Nah mungkin banyak dari pembaca yang bingung website jenis apa ini, jadi sebenarnya saya pun juga masih bingung mau disebut apa atau apa yang mau saya tulis, tutorial ? yah saya aja baru belajar dan walau ada kebisaan tapi gak terlalu ahli untuk bisa buat tutorial paling yang abal-abal(gklah). Nah karena sekarang belum ada rencana mau dijadiin website apa, saya mungkin juga share apapun saya mau :v, biarlah jadi website gado gado, asal jangan CPE (<em>Copy Paste Edit</em>), walau saya juga ngambil bahan dari sumber yang berhamburan di internet, tapi paling tidak saya tidak terlalu malas untuk langsung copy paste (tanpa ngetik manual). Dan yang terpenting saat mengambil bahan diinternet adalah untuk menyertakan sumber dan credit pada yang bersangkutan untuk menghargai usaha orang lain</p>
+<div class="info">
+	<p>baca tulisan saya yang lain, tutorial, artikel maupun cerita di <a href="?menu=blog">blog saya</a></p>
+</div>
+
+<h3>Kenapa buat website ini ?</h3>
+<p>Karena belum nentuin website apa ini, kenapa malah dibuat ? pertama-tama website ini dibuat karena dosen saya <a href="https://www.youtube.com/andiewebdw">pak Andie</a> yang ngasih tugas membuat website dan dihosting ke internet, nah sebenarnya tugasnya buat website pake aplikasi Dreamweaver, tapi karena saya mau belajar buat sendiri dari awal <a href="https://idioms.thefreedictionary.com/from+scratch" target="_blank">(from Scratch)</a>, setelah nyoba buat sendiri (dengan referensi berbagai sumber di internet) dan setelah banyak trial and error, jadilah website ini walaupun belum selesai, tapi saya berniat menyelesaikan website ini(yang masih dalam tahap pengembangan) jadi mohon doa dan dukungannya.</p>
+<div class="info"><p>Jika ada yang berminat memberikan dukungan baik berupa moral maupun modal :v bisa ke halaman <a href="?menu=contact">contact</a></p></div>
+
+<h3>Siapa Saya ?</h3>
+<p>Nah ini yang paling penting (tapi mungkin gk penting buat kalian). Jadi nama saya asli saya Mohamad Albie, tapi lebih baik saya menggunakan nama Muhammad Albie Al Hasan (<a href="">ada alasan khusus</a>), biasanya dipanggil Albie, tapi lebih suka dipanggil Al atau Hasan. Alhamdulillah beragama Islam, lulusan SMKN 5 Banjarmasin, sekarang sedang menempuh jenjang pendidikan tinggi di UNISKA Banjarmasin jurusan Teknik Informatika Semester ke 3. Hobi membaca, berselancar, nonton, dan nulis atau ngetik.</p>
+<div class="info">
+	<p>Lebih lengkap tentang saya di <a href="?menu=about_me">about me</a></p>
+</div>
+
+<h3>Gimana cara buatnya ?</h3>
+<p>Jika ada yang bertanya bagaimana buatnya maka jawabannya dapat ditemukan di berbagai sumber internet, yaitu sebuah website static sederhana akan terdiri dari element HTML dan CSS, dan website responsive yang lebih kompleks dapat menggunakan bahasa pemrograman Javascript, website dinamis dimana website dapat menampilkan kontent yang berubah ubah (terkoneksi dengan database) dan bahasa yang populer digunakan untuk itu adalah PHP dan Mysql sebagai DBMS nya ... <a href="">baca lebih lanjut</a></p>
+<div class="info">
+	<h4>Baca Juga</h4>
+	<p>
+		penjelasan sederhana tentang HTML dan CSS  : <a href="">baca artikel</a>
+	</p>
+	<p>
+		membuat website sederhana dari awal (from scratch) : <a href="">lihat tutorial</a>
+	</p>
+	<p>
+		dasar dasar web development : <a href="">baca artikel</a>
+	</p>
+	<p>
+		cerita saya dalam belajar membuat website : <a href="">ikuti cerita</a>
+	</p>
+</div>
+
+<h3>Apakah yang lain dapat membuat website seperti ini juga ?</h3>
+<p>Kamu juga ingin membuat website seperti ini ?, jangan khawatir kamu pasti bisa ! sebab siapapun yang belajar, berusaha, tekun, dan yang paling penting dengan izin Allah dapat melakukannya, karena itu jangan pernah skeptis saat berniat melakukan sesuatu, tetap belajar dengan diiringi do'a, dan yang terpenting <em>take action</em>.</p>
+<p>Tidak ada yang spesial dari website ini, karena masih sangat sederhana, malah akan terlihat sangat kumuh jika dibandingkan dengan website para mastah diluar sana. Karena itu saya akan <em>keep study and keep action</em> dalam web development</p>
+<p>Saya juga tidak keberatan jika ada bertanya atau ingin bekerjasama dalam pembuatan sebuah website atau <em>aplication programming</em> lainnya, jika ada yang ingin minta buatkan website sederhana dan saya memiliki waktu luang, maka dengan senang hati saya saya akan menerimanya, sebab sebuah skill atau ilmu itu ibarat sebuah pisau, dan terus menggunakannya seperti mengasahnya terus menerus dan membuatnya tajam, dan jika tidak digunakan maka hanya akan berkarat.</p>
+<div class="info">
+	<p>Jika ada yang perlu ditanyakan atau <em>request</em> bisa pilih subject 'Request' di menu contact, atau klik <a href="">disini</a></p>
+	<p>Saya juga sangat senang bekerjasama dengan anda, lets <a href="">kerjasama</a></p>
+	<p>pertanyaan akan saya jawab jika sempat, bagi permintaan pembuatan website akan saya kerjakan jika saya punya waktu luang.</p>
+	<p>bagi yang dengan murah hati mau bedonasi, maka permintaanya akan saya prioritaskan. <a href="">donasi</a></p>
+</div>
+
+<h3>Terima kasih saya</h3>
+<p>Segala puja dan puji syukur kehadirat Allah atas segala rahmat-Nya sehingga website ini dapat <em>tercoding</em> dengan lancar. Saya juga mengucapkan banyak terima kasih kepada guru, dosen, dan teman-teman saya, serta berbagai pihak yang telah berkontribusi dengan memberikan dukungan baik meteri maupun pikirannya.</p>
+<div class="info">
+	<p>Baca lebih lanjut di <a href="?menu=thanks" target="_blank">special thanks</a></p>
+</div>
+
+<h3>Harapan</h3>
+<p>Harapan saya semoga website ini dapat memberikan motivasi bagi saya (<em>khususnya</em>) dan teman teman, dan menjadi tempat berbagi ilmu, pengalaman dan belajar bersama serta untuk menambah pengalaman saya dalam bidang web development (yang mana masih sangat sedikit) dan menambah kepercayaan diri saya untuk berkarya.</p>
+<p>Saya juga berharap ini dapat membuka kesempatan (<em>Oppurtunity</em>) dan menjadi langkah nyata <em>Action</em> dalam meraih mimpi saya (<em>Dream</em>). <em><a href="">#Dream Oppurtunity Action (DOA)</a></em></p>
+<div class="info">
+	<p style="text-align: left;"><em>#Dream Oppurtunity Action</em> adalah <em>qoute</em> teman saya Muhammad Arif Kurniawan, kunjungi websitenya di <a href="http://doa.kelasmuhammad.com">doa.kelasmuhammad.com</a></p>
+</div>
+<h3>Akhir</h3>
+<p>Baik itu saja yang intro singkat yang dapat saya sampaikan (untuk sekarang), terimakasih telah mau membaca hingga selesai, mohon maaf atas kekurangan dan kehilafannya, kritik dan saran dengan senang hati diterima (langsung ke halaman <a href="?menu=contact">contact</a>). Penulis tidak berbakat dalam menyusun kata-kata karena itu sekali lagi mohon maklum, jika mungkin kalimat yang tidak berkenan dihati, maupun gaya penulisan yang kacau karena kurangnya keterampilan penulis. Untuk menjelajah lebih lanjut silahkan pilih menu dipojok kiri atas, sekian dan terima kasih.
+</p>
+<div class="salam"><p class="salam"><span class="italic">Wassalamu'alaikum Warahmatullahi Wabaratuh ....</span><span class="arabic" dir="rtl" lang="ar">السلام عليكم ورحمة ا للة و بركاتة</span></p>
+</div>
+<script>
+// saat wesite selesai diloading
+$(document).ready(function(){
+	alert("Welcome to My Website");
+    $(".btnChangeTxt").click(function(){
+    	// mengambil nilai perubahan dari user
+    	var urlGetTxt = $(".inpChangeTxt").val();
+    	var urlGetClrBg = $(".inpChangeClrBg").val();
+    	var urlGetClrTxt = $(".inpChangeClrTxt").val();
+    	
+    	// mengubah tampilan sesuai yang diinputkan
+    	$(".boxFun span").html(urlGetTxt);
+    	$(".boxFun").css({"background-color":urlGetClrBg});
+    	$(".boxFun span").css({"color":urlGetClrTxt});
+        // alert("Value: " + $(".inpChangeTxt").val());
+    });
+});
+</script>
