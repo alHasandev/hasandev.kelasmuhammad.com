85ee3164eedc1acc4185dbb747a146213b5dc305
Muhammad Albie Al Hasan <alhasan99dev@gmail.com>
2017-12-22 11:50:52 +0800

    Create content_about_me.php
    about me part of hasandev.kelasmuhammad.com

---
+++ content_about_me.php
@@ -0,0 +1,40 @@
+<h2>About Me</h2>
+
+<p>Selengkapnya tentang saya</p>
+
+<h3>Perkenalan</h3>
+
+<p>Perkenalkan nama saya Muhammad Albie Al Hasan (nama asli Mohamad Albie), berasal dari kalimantan selatan, lahir di desa teluk tamba, kec. tabukan, kab. barito kuala pada tanggal 26 april 1999, setelah lulus smp saya pergi ke kota (banjarmasin) untuk menempuh pendidikan di SMKN 5 Banjarmasin dan sekarang telah lulus, melanjutkan belajar di pendidikan tinggi UNISKA Banjarmasin.</p>
+
+<div class="info">
+	<p>Sebenarnya nama saya di dokumen resmi adalah Mohamad Albie, tapi karena (alasan khusus) nama saya itu tidak terlalu sesuai dengan arti yang seharusnya, maka keluarga saya memutuskan mengubah nama saya (tasmiyah) menjadi Muhammad Albie Al Hasan, walaupun di dokumen resmi masih Mohamad Albie karena berbagai masalah mengubah nama dalam dokumen resmi.</p>
+</div>
+<h3>Keluarga</h3>
+
+<div class="readMore">
+	<p>Saya anak kelima dari lima bersaudara (dua perempuan dan tiga laki-laki), ayah telah meninggal, tapi alhamdulillah ibu masih sehat wal afiat. Kakak yang tertua adalah laki-laki bernama Khariyadi, dia lulusan SMAN 1 Tabukan, sekarang berumur 30 tahun, sudah berkeluarga dan bekerja sebagai <em>Admin Security</em> di salah satu lokasi perkebunan perusahaan sawit Hasnur Group, kakak saya yang kedua adalah perempuan bernama Heny Faulida, lulusan LP3I Banjarmasin, sekarang berumur 28 tahun, belum menikah, dan bekerja sebagai admin perusahaan travel dan umroh KALTRABU Banjarmasin, yang ketiga juga perempuan bernama Halidah, berumur 27 tahun, belum menikah, yang keempat adalah laki-laki bernama Arbudi, lulusan SMAN 9 Banjarmasin, sekarang sering melakukan perjalanan keliling <span style="text-decoration: line-through;">dunia</span> kalimantan selatan, yang terakhir adalah saya laki-laki(iya saya laki-laki :v).</p>
+
+	<p>Almarhum ayah saya adalah pensiunan guru sd, beliau kelahiran Amuntai, Hulu sungai selatan, kalsel. Ayah saya meninggal saat saya berumur 3 tahun, yaitu 15 tahun silam, Ibu saya sekarang berumur 55 tahun, selama 15 tahun beliau telah mengurus saya dan saudara saya seorang diri tanpa suami, untuk mengurus 5 anak sendirian pasti sangatlah sulit, untuk memenuhi kebutuan keluarga beliu membuka usaha launddry dirumah, dan juga dari uang pensiunan ayah saya setiap bulannya, dan bagi saya tokoh ayah adalah sesuatu yang tidak dapat saya mengerti, karena waktu itu saya masih sangat kecil dan hampir tidak dapat megingat interaksi apapun dengan ayah, mungkin karena itu ketergantungan saya dengan ibu saya lebih berat. Ibu saya telah membesarkan dan mendidik serta menyekolahkan saya untuk menjadi orang sukes dan memliki akhlak mulia, karena itu saya akan berusaha keras membahagiakan dan memenuhi harapan beliau.</p>
+</div>
+<span class="ReadMore">Read More</span>
+<div class="info">
+	<p>Saya juga punya nenek, paman, dan sepupu lain yang memiliki tempat tinggal yang berbeda, sekarang saya tinggal di rumah sewaan daerah sungai andai dengan ibu, kakak ketiga, keempat, nenek, dan adik (bukan adik kandung). Untuk alamat lebih lengkap lihat dibagian <a href="#alamat">alamat</a></p>
+</div>
+
+<h3 id="alamat">Alamat</h3>
+<h3>Pendidikan</h3>
+<h3>Pekerjaan</h3>
+<h3>Hobi dan Keseharian</h3>
+<p>Keseharian saya selain kuliah, adalah bekerja, jika ada waktu luang saya akan belajar mandiri (lewat internet), kumpul-kumpul bareng teman atau mengeluti hobi saya.</p>
+<p>Hobi saya adalah :</p>
+<div class="hoby">
+<ul>
+	<li>Membaca buku (baik buku komik, novel, maupun buku pelajaran (khusus bidang programming))</li>
+	<li>Nonnto, bukan nonto dibioskop tapi nonto di internet khususnya di <a href="https://youtube.com">Youtube</a></li>
+	<li>Berselancar, bukan berselancar di laut, tapi berselancar di Internet (kalo kata temen saya namanya <em>browsing</em>)</li>
+	<li>Nulis/ngetik, biasanya nulis-nulis tidak karuan, atau ngetik apapun di Ms.Word, tapi sekarang lebih sering mengetikkan code di code editor</li>
+</ul>
+</div>
+<h3>Pengalaman dan Keahlian</h3>
+<h3>Portfolio / Hasil Karya</h3>
+<h3>Social Media</h3>
