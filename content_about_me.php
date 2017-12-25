3886e8bb0851b9b143ea63c350c80c72b5258f57
Muhammad Albie Al Hasan <alhasan99dev@gmail.com>
2017-12-25 09:24:51 +0800

    Add files via upload
    kd tau lagi perubahan ke brp, pusing aing

--- content_about_me.php
+++ content_about_me.php
@@ -16,12 +16,39 @@
 	</div>
 	<div class="unguMuda"></div>
 	<div class="isi unguTua">
+		<div class="imgbox">
+			<img src="img/uniStudent.png" alt="logo Profil Hasandev">
+			<div class="uniska">UNISKA Banjarmasin</div>
+		</div>
 		<div class="singkat titleBio">
 			Nama asli saya adalah Mohamad Albie tapi karena alasan khusus, saya dan keluarga memutuskan menggunakan nama Muhammad Albie Al Hasan, walaupun nama pada dokumen resmi masih Mohamad Albie, biasanya dipanggil Albie tapi saya akan lebih suka jika dipanggil Al atau Hasan, saya anak ke 5 dari 5 bersaudara. Keseharian saya adalah seorang mahasiswa jurusan teknik informatika pada perguruan tinggi UNISKA (Universitas Islam Kalimantan) di banjarmasin. 
 		</div>
-		<div class="titleBio">Hobi</div>
+		
+		<div class="boxModel1">
+			<div class="titleBio block">Pendidikan</div>
+
+			<div class="boxModel2">
+				<div class="statistik1 sd">
+					2004-2010
+				</div>
+				<div class="statistik2 sd">SDN 1 Teluk Tamba</div><br>
+				<div class="statistik1 smp">
+					2010-2013
+				</div>
+				<div class="statistik2 smp">SMPN 1 Tabukan</div><br>
+				<div class="statistik1 smk">
+					2013-2016
+				</div>
+				<div class="statistik2 smk">SMKN 5 Banjarmasin</div><br>
+				<div class="statistik1 kuliah">
+					2016-Sekarang
+				</div>
+				<div class="statistik2 kuliah">UNISKA Banjarmasin</div><br>
+			</div>
+		</div>
+		<span class="clear"></span>
+		<div class="titleBio block">Hobi</div>
 		<div class="hobi">ini hobi</div>
-		<div class="titleBio">Pendidikan</div>
 		<div class="pendidikan">ini pendidikan</div>
 		<div class="titleBio">Pekerjaan</div>
 		<div class="pekerjaan">ini pekerjaan</div>
